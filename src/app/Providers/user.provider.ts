982c768cdb2a45c7e0283d170009a96147cb06d6
hakanpeksen <hknpksn@gmail.com>
2019-06-18 16:17:17 +0300

    fix

--- src/app/Providers/user.provider.ts
+++ src/app/Providers/user.provider.ts
@@ -10,30 +10,29 @@ import 'rxjs/add/operator/map';
 // private _url: string = 'https://front-end-test.azurewebsites.net/api/todo';
 
 // private _eventsUrl: string = 'http://monitera.loggly.com/apiv2/events?rsid=';
-private _searchUrl: string = 'http://monitera.loggly.com/apiv2/search?q=*';
-private _url: string = 'http://monitera.loggly.com/apiv2/events?rsid=';
+
 
 
 constructor(private _http: Http){}
 
-GetRsId(name:string){
+getUsers(uname:string){
 const headers = new Headers();
 let logglyToken = '53ff8abb-9b0e-4cf3-b7d3-bbbce7557d72';
 headers.append('Authorization', `Bearer ${logglyToken}`);
 const opts = new RequestOptions({ headers: headers });
-return this._http.get(this._searchUrl + 'UserName', opts).map((res: Response) => res.json());
+return this._http.get(this._searchUrl + 'uname', opts).map((res: Response) => res.json());
 
 
 }
 
 
-getGetId(){
-const headers = new Headers();
-let logglyToken = '53ff8abb-9b0e-4cf3-b7d3-bbbce7557d72';
-headers.append('Authorization', `Bearer ${logglyToken}`);
-const opts = new RequestOptions({ headers: headers });
-return this._http.get(this._url, opts).map((res: Response) => res.json());
+// getGetId(){
+// const headers = new Headers();
+// let logglyToken = '53ff8abb-9b0e-4cf3-b7d3-bbbce7557d72';
+// headers.append('Authorization', `Bearer ${logglyToken}`);
+// const opts = new RequestOptions({ headers: headers });
+// return this._http.get(this._url, opts).map((res: Response) => res.json());
 
 
-}
+// }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@
 
 import { Component } from '@angular/core';
 import { UserService } from './providers/user.provider';
+import { Http, Response, RequestOptions,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-
+let headers = new Headers();
+//  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+ headers.append('Content-Type','application/x-www-form-urlencoded');
+ let logglyToken = '53ff8abb-9b0e-4cf3-b7d3-bbbce7557d72';
+  headers.append('Authorization',`Bearer ${logglyToken}`);
+let opts = new RequestOptions({ headers: headers});
+import 'rxjs/add/operator/retry';
 @Component({
 selector: 'app-root',
 templateUrl: './app.component.html',
@@ -13,8 +21,14 @@ providers: [UserService]
 
 })
 export class AppComponent {
+  private _searchurl: string = 'http://monitera.loggly.com/apiv2/search?q=';
+private _url: string = 'http://monitera.loggly.com/apiv2/events?';
 post;
 events=[];
+public username:string;
+abc='';
+id:number;
+
 // search;
 // searchid;
 // events;
@@ -24,12 +38,42 @@ events=[];
 // listheadercolor = 'yellow';
 // users = [];
 // count;
-constructor(public _userService: UserService){}
+constructor(public _userService: UserService,private _http: Http){}
+
+
+getUsers(uname: string) {
+  // setTimeout(() => {
+  //       this.isLoading = false;
+  //      }, 2000);
+  
+         console.clear();
+           this._http.get(this._searchurl + uname.replace(/\%C4%B1/g,'ı'),opts)
+           .retry(2)
+           .map(r => r.json())
+               .subscribe(res => { this.post = res;
+                 this._http.get(this._url + 'rsid=' + res.rsid.id, opts)
+                 .map(r => r.json())
+                 .subscribe(res => { this.post = res;
+                  this.abc= this.post.total_events;
+                  });
+
+
+                    });
+
+
+                      //  this.abc =res[0]);
+                      //  error => console.log('401 hatası' +  error ),
+                      //  () => console.log('hepsi tamamlandı')
+                      //  );  // 2 .subscribe
+                  //  });   // 1 .subscribe
+
+                }
+
 ngOnInit(){
-  this._userService.GetRsId(name)
-.subscribe(res => this.post = res);
-  this._userService.getGetId()
-  .subscribe(resPostData => this.post = resPostData);
+//   this._userService.getUsers(uname:string)
+// .subscribe(res => this.post = res);
+//   this._userService.getGetId()
+//   .subscribe(resPostData => this.post = resPostData);
   
     }
   }
